e4f2cb218af757b0d9341035b48cbbaad6f22f48
javier14-dev <jcondorich@unjbg.edu.pe>
2026-02-22 18:02:32 -0500

    Refactor Usuario: implementa composicion con Curso

--- src/domain/Usuario.ts
+++ src/domain/Usuario.ts
@@ -1,25 +1,38 @@
+import { Curso } from "./Curso";
+
 export class Usuario {
     private id: string;
     private nombre: string;
     private email: string;
-    private cursos: string[];
+    private cursos: Curso[] = [];
 
     constructor(id: string, nombre: string, email: string) {
         this.id = id;
         this.nombre = nombre;
         this.email = email;
-        this.cursos = [];
     }
 
-    public agregarCurso(curso: string): void {
+    getId(): string {
+        return this.id;
+    }
+
+    getNombre(): string {
+        return this.nombre;
+    }
+
+    getEmail(): string {
+        return this.email;
+    }
+
+    public agregarCurso(curso: Curso): void {
         this.cursos.push(curso);
     }
 
-    public obtenerCursos(): string[] {
+    public obtenerCursos(): Curso[] {
         return this.cursos;
     }
 
-    public obtenerInformacion(): string {
-        return `Usuario: ${this.nombre} - Email: ${this.email}`;
+    buscarCursoPorNombre(nombre: string): Curso | undefined {
+        return this.cursos.find(curso => curso.getNombre() === nombre);
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-import { Usuario } from "./domain/Usuario";
 
-const usuario1 = new Usuario("1", "Javier", "jcondorich@unjbg.edu.pe");
-
-usuario1.agregarCurso("Programación Avanzada");
-
-console.log(usuario1.obtenerInformacion());
-console.log(usuario1.obtenerCursos());
